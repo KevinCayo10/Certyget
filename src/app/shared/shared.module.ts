800b4d6f2f2f72d44f671b72b0ed51a18c858a6b
ErikGranda3756 <egranda3756@uta.edu.ec>
2023-11-27 21:12:03 -0500

    feat: Implementa el componente paginator

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,6 +8,7 @@ import { TitleComponent } from './components/title/title.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatPaginatorModule } from '@angular/material/paginator';
 @NgModule({
   declarations: [
     ContainerComponent,
@@ -16,7 +17,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     TableComponent,
     TitleComponent,
   ],
-  imports: [CommonModule, MatCardModule, MatIconModule, MatToolbarModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatPaginatorModule,
+  ],
   exports: [
     ContainerComponent,
     KeypadButtonComponent,
