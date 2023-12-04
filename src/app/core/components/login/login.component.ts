9988124390b186ffe0a4bad9129bec8288b8accb
ErikGranda3756 <egranda3756@uta.edu.ec>
2023-12-03 19:36:04 -0500

    feat: Integra servicio de login con el respectivo front

--- src/app/core/components/login/login.component.ts
+++ src/app/core/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'cer-login',
@@ -8,13 +9,24 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class LoginComponent {
   togglePassword = true;
-  constructor(private builder: FormBuilder) {
+  constructor(private builder: FormBuilder, private authService: AuthService) {
     sessionStorage.clear();
   }
   userdata: any;
   loginForm = this.builder.group({
-    username: this.builder.control('', Validators.required),
-    password: this.builder.control('', Validators.required),
+    user_usu: this.builder.control('', Validators.required),
+    pass_usu: this.builder.control('', Validators.required),
   });
-  proceedLogin() {}
+  proceedLogin() {
+    if (this.loginForm.valid) {
+      this.authService
+        .login({
+          user_usu: this.loginForm.value.user_usu,
+          pass_usu: this.loginForm.value.pass_usu,
+        })
+        .subscribe((res) => {
+          console.log(res);
+        });
+    }
+  }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    PAGE_SIZE: 5
+  PAGE_SIZE: 5,
+  endpoint: 'http://localhost:4000',
 };
