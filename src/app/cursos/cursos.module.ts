55b2847eb99d7199ff81889edc1d64393b55e470
Kevin CT <kcayo6155@uta.edu.ec>
2023-12-14 09:46:46 -0500

    refactor: Corrige los estilos y campos de los modulos de categorias y usuarios

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -5,16 +5,14 @@ import { CursosRoutingModule } from './cursos-routing.module';
 import { PageListComponent } from './pages/page-list/page-list.component';
 import { SharedModule } from '../shared/shared.module';
 import { FormComponent } from './components/form/form.component';
-
+import { MatDatepickerModule } from '@angular/material/datepicker';
 @NgModule({
-  declarations: [
-    PageListComponent,
-    FormComponent
-  ],
+  declarations: [PageListComponent, FormComponent],
   imports: [
     CommonModule,
     CursosRoutingModule,
-    SharedModule
-  ]
+    SharedModule,
+    MatDatepickerModule,
+  ],
 })
-export class CursosModule { }
+export class CursosModule {}
