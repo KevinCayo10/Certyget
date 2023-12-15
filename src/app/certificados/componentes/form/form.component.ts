82dc5960bf7daf87004710e779b93f1ffe218e6b
Kevin CT <kcayo6155@uta.edu.ec>
2023-12-15 09:16:28 -0500

    feat: Implementa cargar csv y mostrar en la tabla

--- src/app/certificados/componentes/form/form.component.ts
+++ src/app/certificados/componentes/form/form.component.ts
@@ -3,6 +3,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { MetaDataColumn } from 'src/app/shared/interfaces/metacolumn.interfaces';
 import { environment } from 'src/environments/environment.development';
 import { PageListComponent } from '../../pages/page-list/page-list.component';
+import { read, utils } from 'xlsx';
 
 @Component({
   selector: 'cer-form',
@@ -11,76 +12,18 @@ import { PageListComponent } from '../../pages/page-list/page-list.component';
 })
 export class FormComponent {
   allData: any[] = []; // Agrega una referencia a todos los datos
-  data: any[] = [
-    {
-      id_cate: 1,
-      fecha_gen_cer: '2023-01-01',
-      ced_par: '1234567890',
-      email_par: 'example@email.com',
-      url_gen_cer: 'certificado1.pdf',
-      estado_cer: 'Aprobado',
-    },
-    {
-      id_cate: 2,
-      fecha_gen_cer: '2023-01-02',
-      ced_par: '9876543210',
-      email_par: 'another@example.com',
-      url_gen_cer: 'certificado2.pdf',
-      estado_cer: 'Pendiente',
-    },
-    {
-      id_cate: 2,
-      fecha_gen_cer: '2023-01-02',
-      ced_par: '9876543210',
-      email_par: 'another@example.com',
-      url_gen_cer: 'certificado2.pdf',
-      estado_cer: 'Pendiente',
-    },
-    {
-      id_cate: 2,
-      fecha_gen_cer: '2023-01-02',
-      ced_par: '9876543210',
-      email_par: 'another@example.com',
-      url_gen_cer: 'certificado2.pdf',
-      estado_cer: 'Pendiente',
-    },
-    {
-      id_cate: 2,
-      fecha_gen_cer: '2023-01-02',
-      ced_par: '9876543210',
-      email_par: 'another@example.com',
-      url_gen_cer: 'certificado2.pdf',
-      estado_cer: 'Pendiente',
-    },
-    {
-      id_cate: 2,
-      fecha_gen_cer: '2023-01-02',
-      ced_par: '9876543210',
-      email_par: 'another@example.com',
-      url_gen_cer: 'certificado2.pdf',
-      estado_cer: 'Pendiente',
-    },
-    {
-      id_cate: 2,
-      fecha_gen_cer: '2023-01-02',
-      ced_par: '9876543210',
-      email_par: 'another@example.com',
-      url_gen_cer: 'certificado2.pdf',
-      estado_cer: 'Pendiente',
-    },
-
-    // Agrega más objetos según sea necesario
-  ];
+  data: any[] = [];
   totalRecords = 0;
   title = sessionStorage.getItem('selectedCursoNom');
 
   metaDataColumns: MetaDataColumn[] = [
-    { field: 'id_cate', title: 'ID' },
-    { field: 'fecha_gen_cer', title: 'FECHA EMITIDA' },
-    { field: 'ced_par', title: 'CEDULA PARTICIPANTE' },
-    { field: 'email_par', title: 'EMAIL PARTICIPANTE' },
-    { field: 'url_gen_cer', title: 'CERTIFICADO' },
-    { field: 'estado_cer', title: 'ESTADO' },
+    { field: 'ced_par', title: 'CEDULA' },
+    { field: 'nom_pat_par', title: '1ER NOMBRE' },
+    { field: 'nom_mat_par', title: '2DO NOMBRE' },
+    { field: 'ape_pat_par', title: '1ER APELLIDO' },
+    { field: 'ape_mat_par', title: '2DO APELLIDO' },
+    { field: 'email_par', title: 'EMAIL' },
+    { field: 'telf_par', title: 'TELEFONO' },
   ];
 
   constructor(
@@ -90,7 +33,7 @@ export class FormComponent {
 
   ngOnInit(): void {
     // Puedes agregar lógica de inicialización si es necesario
-    this.loadParticipantes();
+    // this.loadParticipantes();
   }
 
   saveData() {
@@ -111,7 +54,25 @@ export class FormComponent {
     this.data = this.allData.slice(startIndex, startIndex + pageSize);
     this.totalRecords = this.allData.length;
   }
+  users: any[] = [];
+  csvImport($event: any) {
+    const files = $event.target.files;
+    if (files.length) {
+      const file = files[0];
+      const reader = new FileReader();
+      reader.onload = (event: any) => {
+        const wb = read(event.target.result);
+        const sheets = wb.SheetNames;
 
+        if (sheets.length) {
+          const row = utils.sheet_to_json(wb.Sheets[sheets[0]]);
+          this.data = row;
+          console.log('DATA : ', this.data);
+        }
+      };
+      reader.readAsArrayBuffer(file);
+    }
+  }
   loadParticipantes() {
     console.log(sessionStorage.getItem('selectedCursoId'));
     // Puedes cargar datos dinámicamente si es necesario
