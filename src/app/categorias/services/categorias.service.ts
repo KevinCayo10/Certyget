ddd9ab41c5a808b42e1b81e8b0e17c117883e504
Kevin CT <kcayo6155@uta.edu.ec>
2024-01-13 23:36:13 -0500

    refacor: corrige la rutas con POO

--- src/app/categorias/services/categorias.service.ts
+++ src/app/categorias/services/categorias.service.ts
@@ -1,37 +1,48 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CategoriasService {
-  private baseUrl = 'http://localhost:3000';
+  private myAppUrl: string;
+  private myApiUrlCategorias: string;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.myAppUrl = environment.endpoint;
+    this.myApiUrlCategorias = '/api/category/';
+  }
 
   // Obtener todas las categorías
   getCategorias(): Observable<any> {
-    return this.http.get(`${this.baseUrl}/`);
+    return this.http.get(`${this.myAppUrl}${this.myApiUrlCategorias}`);
   }
 
   // Obtener una categoría por su id
   getCategoriaById(id: number): Observable<any> {
-    return this.http.get(`${this.baseUrl}/${id}`);
+    return this.http.get(`${this.myAppUrl}${this.myApiUrlCategorias}/${id}`);
   }
 
   // Crear una nueva categoría
   createCategoria(categoria: any): Observable<any> {
-    return this.http.post(`${this.baseUrl}/`, categoria);
+    return this.http.post(
+      `${this.myAppUrl}${this.myApiUrlCategorias}/`,
+      categoria
+    );
   }
 
   // Actualizar una categoría
   updateCategoria(id: number, categoria: any): Observable<any> {
-    return this.http.put(`${this.baseUrl}/${id}`, categoria);
+    return this.http.put(
+      `${this.myAppUrl}${this.myApiUrlCategorias}/${id}`,
+      categoria
+    );
   }
 
   // Eliminar una categoría
   deleteCategoria(id: number): Observable<any> {
-    return this.http.delete(`${this.baseUrl}/${id}`);
+    return this.http.delete(`${this.myAppUrl}${this.myApiUrlCategorias}/${id}`);
   }
 }
